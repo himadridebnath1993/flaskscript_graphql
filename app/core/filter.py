fba586c40356256674c4cc5f660536435a35c8b1
himadri <himadri.d@cogostech.com>
2022-01-13 20:43:05 +0530

    filter bug fix

--- app/core/filter.py
+++ app/core/filter.py
@@ -10,35 +10,41 @@ async def accounting_invoice(_data: json) -> list:
     _array = []
     for accounting in _data['accounting_invoice']:
         for invoice in accounting['invoice_details']:
-            _object = get_key_value(invoice, {})
+            _object = get_key_value(invoice, {}, _accConstList)
+            for skey in _accConstList:
+                if skey not in _object:
+                    _object[skey] = ''
             _array.append(_object)
     return _array
 
 
-_driverPaymentList = ['cid']
+_driverPaymentList =['cid', 'unit','payout_settlement_id','approved_amount','total','subtotal']
 
 
 async def accounting_driver_payment(_data: json) -> list:
     _array = []
     for accounting in _data['accounting_driver_payment']:
-        _object = get_key_value(accounting, {})
+        _object = get_key_value(accounting, {}, _driverPaymentList)
+        for skey in _driverPaymentList:
+            if skey not in _object:
+                _object[skey] = ''
         _array.append(_object)
     return _array
 
 
-def get_key_value(_data: json, _object: dict) -> dict:
+def get_key_value(_data: json, _object: dict, params: list) -> dict:
     for key, value in _data.items():
         try:
-            if key in _accConstList:
+            if key in params:
                 _object[key] = value
             else:
                 if isinstance(value, dict):
                     print("isinstance : dict")
-                    get_key_value(value, _object)
+                    get_key_value(value, _object,params)
                 elif isinstance(value, list):
                     print("isinstance : list")
                     if all(not isinstance(x, int) for x in value) and not not value:
-                        get_key_value(value[0], _object)
+                        get_key_value(value[0], _object,params)
         except Exception as e:
             print(e)
 
